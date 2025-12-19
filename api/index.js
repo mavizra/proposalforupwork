0a80470c6fe0470c12fe47d5db09abddd9b3382a
mavizra <marvizra@gmail.com>
2025-12-19 15:17:45 +0200

    index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import postgres from "postgres";
 import listingsRouter from "./routes/listings";
 import listingRelatedRouter from "./routes/listing-related";
-import { mockBooks as mockListings } from "./lib/mockData";
+import { mockListings } from "./lib/mockData";
 
 const app = new Hono();
 
